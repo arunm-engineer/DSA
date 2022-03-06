976732aa9e5dae6373e28595eafa11200459305e
Arun <arun8778451315@gmail.com>
2022-03-06 23:51:23 +0530

    Update

--- DSA_L2/DP/l004_Target.java
+++ DSA_L2/DP/l004_Target.java
@@ -108,6 +108,30 @@ public class l004_Target {
         return dp[TAR];
     }
 
+    public static void Target() {
+        int[] arr = {2, 3, 5, 7};
+        int n = arr.length, tar = 10;
+
+        // Permutations
+        // int[] dp = new int[tar+1];
+        // Arrays.fill(dp, -1);
+
+        // System.out.println(infinitePerm_mem(arr, tar, dp));
+        // System.out.println(infinitePerm_tab(arr, tar, dp));
+
+        // Combinations
+        // int[][] dp = new int[n][tar+1];
+        // for (int[] a : dp)
+            // Arrays.fill(a, -1);
+
+        // System.out.println(infiniteComb_mem(arr, n-1, tar, dp));
+        // System.out.println(infiniteComb_tab(arr, n-1, tar, dp));
+        // int[] dp = new int[tar+1];
+        // Arrays.fill(dp, -1);
+        // System.out.println(infiniteComb_opt(arr, n-1, tar, dp));
+        // display(dp);
+    }
+
     // ===================================================
 
     // LC 322 
@@ -119,7 +143,7 @@ public class l004_Target {
         int[] dp = new int[amount+1];
         Arrays.fill(dp, (int) 1e9);
         
-        int ans = infiniteComb_opt(coins, n-1, amount, dp);
+        int ans = infiniteComb_opt_00(coins, n-1, amount, dp);
         return ans == (int) 1e9 ? -1 : ans;
     }
     
@@ -183,35 +207,302 @@ public class l004_Target {
 
     // ===================================================
 
+    // https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
+    public static Boolean isSubsetSum(int N, int arr[], int sum) {
+        // Since T, F both are part of answer we use int DP and not boolean DP
+        int[][] dp = new int[N+1][sum+1];
+        for (int[] a : dp)
+            Arrays.fill(a, -1);
+            
+        // int ans = subsetSum_mem(arr, N, sum, dp);
+        int ans = subsetSum_tab(arr, N, sum, dp);
+        // System.out.println(subsetSum_BackEng(dp, N, sum, arr, "")); // manipulate according to the type of DP you pass => int DP or boolean DP
 
+        return ans == 1;
+    }
+    
+    // 1 => indicates subset exists, 0 => indicates subset not exists
+    public static int subsetSum_mem(int[] arr, int n, int tar, int[][] dp) {
+        if (tar == 0 || n == 0)
+            return dp[n][tar] = tar == 0 ? 1 : 0;
+            
+        if (dp[n][tar] != -1)
+            return dp[n][tar];
+            
+        boolean res = false;
+        
+        if (tar - arr[n-1] >= 0)
+            res = res || subsetSum_mem(arr, n-1, tar - arr[n-1], dp) == 1; // yes
+        res = res || subsetSum_mem(arr, n-1, tar, dp) == 1; // no
+        
+        return dp[n][tar] = res ? 1 : 0;
+    }
+    
+    public static int subsetSum_tab(int[] arr, int N, int TAR, int[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int tar = 0; tar <= TAR; tar++) {
+                if (tar == 0 || n == 0) {
+                    dp[n][tar] = tar == 0 ? 1 : 0;
+                    continue;
+                }
+                
+                boolean res = false;
+                
+                if (tar - arr[n-1] >= 0)
+                    res = res || dp[n-1][tar - arr[n-1]] == 1; // yes
+                res = res || dp[n-1][tar] == 1; // no
+                
+                dp[n][tar] = res ? 1 : 0;
+            }
+        }
+        
+        return dp[N][TAR];
+    }
 
-    public static void Target() {
-        int[] arr = {2, 3, 5, 7};
-        int n = arr.length, tar = 10;
+    // Back Engineering => To print the coins used
+    // For subset we used int DP, here we don't have as such need of int DP, we can work with boolean DP as well
+    // If want to use int DP can be done just manipulate 1 => T, 0 => F
+    // Manipulate accordingly for boolean DP or int DP
+    public static int subsetSum_BackEng(boolean[][] dp, int n, int tar, int[] arr, String path) {
+        if (tar == 0) {
+            System.out.println(path);
+            return 1;
+        }
 
-        // Permutations
-        // int[] dp = new int[tar+1];
-        // Arrays.fill(dp, -1);
+        if (dp[n][tar] == false) // False indicates => not found a combination to form "w"
+            return 0;
 
-        // System.out.println(infinitePerm_mem(arr, tar, dp));
-        // System.out.println(infinitePerm_tab(arr, tar, dp));
+        int count = 0;
+        if (tar - arr[n-1] >= 0)
+            count += subsetSum_BackEng(dp, n-1, tar - arr[n-1], arr, arr[n-1] + " " + path);
+        count += subsetSum_BackEng(dp, n-1, tar, arr, path);
 
-        // Combinations
-        // int[][] dp = new int[n][tar+1];
+        return count;
+    }
+
+    // ===================================================
+
+    // Knapsack 01 => https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+    // Function to return max value that can be put in knapsack of capacity W.
+    public static int knapSack(int W, int wt[], int val[], int n) {
+        int[][] dp = new int[n+1][W+1];
+        for (int[] a : dp)
+            Arrays.fill(a, -1);
+            
+        // return knapsack_01_mem(W, n, wt, val, dp);
+        return knapsack_01_tab(W, n, wt, val, dp);
+    }
+    
+    public static int knapsack_01_mem(int w, int n, int[] wt, int[] val, int[][] dp) {
+        if (w == 0 || n == 0) 
+            return dp[n][w] = 0;
+        
+        if (dp[n][w] != -1)
+            return dp[n][w];
+            
+        int max = 0;
+        if (w - wt[n-1] >= 0) 
+            max = Math.max(max, knapsack_01_mem(w - wt[n-1], n-1, wt, val, dp) + val[n-1]);
+        max = Math.max(max, knapsack_01_mem(w, n-1, wt, val, dp));
+        
+        return dp[n][w] = max;
+    }
+    
+    public static int knapsack_01_tab(int W, int N, int[] wt, int[] val, int[][] dp) {
+        
+        for (int n = 0; n <= N; n++) {
+            for (int w = 0; w <= W; w++) {
+                if (w == 0 || n == 0) {
+                    dp[n][w] = 0;
+                    continue;
+                }
+                
+                int max = 0;
+                if (w - wt[n-1] >= 0) 
+                    max = Math.max(max, dp[n-1][w - wt[n-1]] + val[n-1]);
+                max = Math.max(max, dp[n-1][w]);
+                
+                dp[n][w] = max;
+            }
+        }
+        
+        return dp[N][W];
+    }
+
+    // ===================================================
+
+    // https://www.geeksforgeeks.org/unbounded-knapsack-repetition-items-allowed/
+    // Unbounded Knapsack repetition allowed => infinite coins
+    // Same Classical knapsack 01 problem just single change in "yes" (incl) calls => "n" can appear again
+    public static int knapSack(int N, int W, int val[], int wt[]) {
+        // Subsq method => infinite comb => Correct but uses 2D DP space
+        // int[][] dp = new int[N+1][W+1];
         // for (int[] a : dp)
-            // Arrays.fill(a, -1);
+        //     Arrays.fill(a, -1);
+            
+        // return unboundedKnapsack_mem_01(W, N, wt, val, dp);
+        // return unboundedKnapsack_tab_01(W, N, wt, val, dp);
 
-        // System.out.println(infiniteComb_mem(arr, n-1, tar, dp));
-        // System.out.println(infiniteComb_tab(arr, n-1, tar, dp));
-        // int[] dp = new int[tar+1];
-        // Arrays.fill(dp, -1);
-        // System.out.println(infiniteComb_opt(arr, n-1, tar, dp));
-        // display(dp);
+        // nCr method => infinite perm (can also do comb also, no change in ans) => Space Optimized 1D DP
+        int[] dp = new int[W+1];
+        Arrays.fill(dp, -1);
+        
+        // return unboundedKnapsack_mem_02(W, wt, val, dp);
+        return unboundedKnapsack_tab_02(W, wt, val, dp);
+        
+    }
+    
+    public static int unboundedKnapsack_mem_01(int w, int n, int[] wt, int[] val, int[][] dp) {
+        if (w == 0 || n == 0) 
+            return dp[n][w] = 0;
+        
+        if (dp[n][w] != -1)
+            return dp[n][w];
+            
+        int max = 0;
+        if (w - wt[n-1] >= 0) 
+            max = Math.max(max, unboundedKnapsack_mem_01(w - wt[n-1], n, wt, val, dp) + val[n-1]);
+        max = Math.max(max, unboundedKnapsack_mem_01(w, n-1, wt, val, dp));
+        
+        return dp[n][w] = max;
+    }
+    
+    public static int unboundedKnapsack_tab_01(int W, int N, int[] wt, int[] val, int[][] dp) {
+        
+        for (int n = 0; n <= N; n++) {
+            for (int w = 0; w <= W; w++) {
+                if (w == 0 || n == 0) {
+                    dp[n][w] = 0;
+                    continue;
+                }
+                
+                int max = 0;
+                if (w - wt[n-1] >= 0) 
+                    max = Math.max(max, dp[n][w - wt[n-1]] + val[n-1]);
+                max = Math.max(max, dp[n-1][w]);
+                
+                dp[n][w] = max;
+            }
+        }
+        
+        return dp[N][W];
+    }
+
+    // Space Optimized 1D DP used => Infinite perm
+    public static int unboundedKnapsack_mem_02(int w, int[] wt, int[] val, int[] dp) {
+        if (w == 0)
+            return dp[w] = 0;
+        
+        if (dp[w] != -1)
+            return dp[w];
+            
+        int max = 0;
+        for (int i = 0; i < wt.length; i++) {
+            if (w - wt[i] >= 0) {
+                int recAns = unboundedKnapsack_mem_02(w - wt[i], wt, val, dp) + val[i];
+                max = Math.max(max, recAns);
+            }
+        }
+        
+        return dp[w] = max;
+    }
+    
+    public static int unboundedKnapsack_tab_02(int W, int[] wt, int[] val, int[] dp) {
+        for (int w = 0; w <= W; w++) {
+            if (w == 0) {
+                dp[w] = 0;
+                continue;
+            }
+                
+            int max = 0;
+            for (int i = 0; i < wt.length; i++) {
+                if (w - wt[i] >= 0) {
+                    int recAns = unboundedKnapsack_mem_02(w - wt[i], wt, val, dp) + val[i];
+                    max = Math.max(max, recAns);
+                }
+            }
+            
+            dp[w] = max;
+        }
+        
+        return dp[W];
     }
 
     // ===================================================
     
+    // https://www.geeksforgeeks.org/find-number-of-solutions-of-a-linear-equation-of-n-variables/
+    // Same infinite comb => just try to relate it with infinite comb
+    public static int infiniteComb_opt_01(int[] coeff, int N, int RHS, int[] dp) {
+        int TAR = RHS;
+        int[] arr = coeff;
+        
+        for (int n = 0; n <= N; n++) {
+            for (int tar = 0; tar <= TAR; tar++) {
+                if (tar == 0) {
+                    dp[tar] = 1;
+                    continue;
+                }
+                
+                // Here we don't have to run loop for (n-1) coins since they are already pre-calculated, so just calculate for nth coin
+                if (tar - arr[n] >= 0)
+                dp[tar] += dp[tar - arr[n]];
+            }
+        }
+        
+        return dp[TAR];
+    }
+
+    // ===================================================
+
+    // LC 416
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums)
+            sum += num;
+        
+        // We cant find a subset for "odd" sum, since half of odd sum will be decimal val, for which subset cannot exist
+        if (sum%2 != 0) 
+            return false;
+        
+        // We now only need to find the subset for next half to equal sum/2 which together forms "sum"
+        int n = nums.length, tar = sum/2;
+        boolean[][] dp = new boolean[n+1][tar+1];
+        
+        return subsetSum_tab_00(nums, n, tar, dp);
+    }
+    
+    // Same target sum i.e. Subset Sum approach
+    public static boolean subsetSum_tab_00(int[] arr, int N, int TAR, boolean[][] dp) {
+        for (int n = 0; n <= N; n++) {
+            for (int tar = 0; tar <= TAR; tar++) {
+                if (tar == 0 || n == 0) {
+                    dp[n][tar] = tar == 0 ? true : false;
+                    continue;
+                }
+                
+                boolean res = false;
+                
+                if (tar - arr[n-1] >= 0)
+                    res = res || dp[n-1][tar - arr[n-1]]; // yes
+                res = res || dp[n-1][tar]; // no
+                
+                dp[n][tar] = res;
+            }
+        }
+        
+        return dp[N][TAR];
+    }
+
+    // ===================================================
+    
+    
     public static void main(String[] args) {
-        Target();
+        // Target();
+        
+        int N = 3, sum = 4;
+        int[] arr = {2, 2, 3};
+        // isSubsetSum(N, arr, sum);
+        int[] dp = new int[sum+1];
+        System.out.println(infiniteComb_opt_01(arr, N-1, sum, dp));
     }
 }
